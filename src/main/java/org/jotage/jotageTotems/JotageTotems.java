771c19e7dea53b9069a5eb707be35d4ad8a1778d
JotaPePG <obmitimbo@gmail.com>
2025-12-07 20:46:14 -0300

    Inicial Commit

---
+++ src/main/java/org/jotage/jotageTotems/JotageTotems.java
@@ -0,0 +1,17 @@
+package org.jotage.jotageTotems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JotageTotems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
